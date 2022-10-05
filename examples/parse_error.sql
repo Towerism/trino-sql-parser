8e1c86384dc04c1f81812217d326a777938ea701
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2022-10-05 11:22:25 -0500

    Add tests

---
+++ examples/parse_error.sql
@@ -0,0 +1,1 @@
+this will result in a parse error
---
+++ test.mjs
@@ -0,0 +1,26 @@
+import test from 'ava'
+
+import fs from 'fs'
+import { URL } from 'url'
+
+import { parse } from './dist/main.mjs'
+
+function getExamples() {
+  const examplesPath = new URL('./examples', import.meta.url).pathname;
+  return fs.readdirSync(examplesPath).map((exampleFile) => {
+    const exampleFilePath = new URL(`./examples/${exampleFile}`, import.meta.url).pathname;
+    const buffer = fs.readFileSync(exampleFilePath)
+    return [exampleFile, buffer.toString()]
+  })
+}
+
+getExamples().forEach(([sqlFileName, sql]) => {
+  test(sqlFileName, (t) => {
+    if (sqlFileName === 'parse_error.sql') {
+      t.throws(() => parse(sql))
+      return
+    }
+    parse(sql)
+    t.pass()
+  })
+})
